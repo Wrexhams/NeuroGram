9ec442dbc904126dad74fe896e1121eda0ee2663
Wrexhams <tr3verstlasow@gmail.com>
2026-01-08 19:21:14 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = list(map(int, os.getenv("ADMINS", "").split(","))) if os.getenv("ADMINS") else []
